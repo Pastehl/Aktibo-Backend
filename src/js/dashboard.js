d884cb1713c3932c183dfe8a68a0592431b1ca19
Pastehl <maaashieon@gmail.com>
2023-11-20 22:08:01 +0800

    Latest working

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,48 +1,57 @@
-
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 import { getAuth, onAuthStateChanged, signOut } from "firebase/auth";
-import { getFirestore, collection, query, where, getDocs, doc, setDoc, updateDoc, getDoc, increment, orderBy, limit, startAfter} from "firebase/firestore";
-import * as bootstrap from 'bootstrap'
-import * as Chart from 'chart.js'
-
+import {
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+  doc,
+  setDoc,
+  updateDoc,
+  getDoc,
+  increment,
+  orderBy,
+  limit,
+  startAfter,
+  arrayUnion,
+  arrayRemove
+} from "firebase/firestore";
+import * as bootstrap from "bootstrap";
+import "../scss/styles.scss";
+import { main, start } from "@popperjs/core";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAH168KKUYGhSGV_GVX5SqDGfxm4vtYR7w",
-    authDomain: "aktibo-2023.firebaseapp.com",
-    projectId: "aktibo-2023",
-    storageBucket: "aktibo-2023.appspot.com",
-    messagingSenderId: "363113385770",
-    appId: "1:363113385770:web:bdf8d66757fd2067b8d853",
-    measurementId: "G-1VTRRK1T20"
+  apiKey: "AIzaSyAH168KKUYGhSGV_GVX5SqDGfxm4vtYR7w",
+  authDomain: "aktibo-2023.firebaseapp.com",
+  projectId: "aktibo-2023",
+  storageBucket: "aktibo-2023.appspot.com",
+  messagingSenderId: "363113385770",
+  appId: "1:363113385770:web:bdf8d66757fd2067b8d853",
+  measurementId: "G-1VTRRK1T20",
 };
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
 const db = getFirestore(app);
 
-
-import '../scss/styles.scss';
-
+// redirect user if user is NOT signed in
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in, see docs for a list of available properties
-      // https://firebase.google.com/docs/reference/js/auth.user
-      const uid = user.uid;
-      // ...
-    } else {
-      // User is signed out
-      // ...
+  if (user) {
+    // User is signed in
+  } else {
+    window.location.href = "index.html";
+  }
+});
+
+// logout
+document.getElementById("logout_btn").addEventListener("click", function () {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
       window.location.href = "index.html";
-    }
-  });
-
-document.getElementById("logout_btn").addEventListener("click",function(){
-    signOut(auth).then(() => {
-        // Sign-out successful.
-        window.location.href = "index.html";
-
-      }).catch((error) => {
-        // An error happened.
-      });
-      
-})
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+});
