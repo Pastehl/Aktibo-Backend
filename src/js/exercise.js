8a8fedfcd76d38b8be6656b9a542e861d0b17b15
unknown <limjade7.24@gmail.com>
2023-11-20 19:42:10 +0800

    fixed dropdown, background and added front end modal with tagging

--- src/js/exercise.js
+++ src/js/exercise.js
@@ -41,8 +41,90 @@ document.getElementById("logout_btn").addEventListener("click",function(){
       
 })
 
-document.getElementById('newExerciseButton').addEventListener('click',function(){
-    window.location.href = "exerciseForms.html";
+// document.getElementById('newExerciseButton').addEventListener('click',function(){
+//     window.location.href = "exerciseForms.html";
 
-})
+// })
 
+//modal
+var modal = document.getElementById("myModal");
+var openModalBtn = document.getElementById("addbtn");
+var closeModalBtn = document.getElementsByClassName("close")[0];
+var cancelButton = document.querySelector(".cancel-button");
+
+openModalBtn.onclick = function () {
+  modal.style.display = "block";
+}
+
+closeModalBtn.onclick = function () {
+  modal.style.display = "none";
+}
+
+cancelButton.onclick = function () {
+  modal.style.display = "none";
+}
+
+// Handle form submission
+var exerciseForm = document.getElementById("exerciseForm");
+
+exerciseForm.addEventListener("submit", function (event) {
+  event.preventDefault(); // Prevent the default form submission
+  // You can add code to handle form submission here, e.g., sending data to a server
+  modal.style.display = "none"; // Close the modal after submission
+});
+
+
+const ul = document.querySelector("tag-list"),
+input = document.querySelector("tag-input"),
+tagNumb = document.querySelector(".details span");
+
+let maxTags = 10,
+tags = ["coding", "nepal"];
+
+countTags();
+createTag();
+
+function countTags(){
+    input.focus();
+    tagNumb.innerText = maxTags - tags.length;
+}
+
+function createTag(){
+    ul.querySelectorAll("li").forEach(li => li.remove());
+    tags.slice().reverse().forEach(tag =>{
+        let liTag = `<li>${tag} <i class='bx bxs-x-circle' onclick="remove(this, '${tag}')"></i></li>`;
+        ul.insertAdjacentHTML("afterbegin", liTag);
+    });
+    countTags();
+}
+
+function remove(element, tag){
+    let index  = tags.indexOf(tag);
+    tags = [...tags.slice(0, index), ...tags.slice(index + 1)];
+    element.parentElement.remove();
+    countTags();
+}
+
+function addTag(e){
+    if(e.key == "Enter"){
+        let tag = e.target.value.replace(/\s+/g, ' ');
+        if(tag.length > 1 && !tags.includes(tag)){
+            if(tags.length < 10){
+                tag.split(',').forEach(tag => {
+                    tags.push(tag);
+                    createTag();
+                });
+            }
+        }
+        e.target.value = "";
+    }
+}
+
+input.addEventListener("keyup", addTag);
+
+const removeBtn = document.querySelector(".details button");
+removeBtn.addEventListener("click", () =>{
+    tags.length = 0;
+    ul.querySelectorAll("li").forEach(li => li.remove());
+    countTags();
+});
