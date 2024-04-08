2aadc5134dfe8a015ed4ab5306db74fbd36434cc
Pastehl <maaashieon@gmail.com>
2024-04-08 17:34:04 +0800

    Modifed userMoments

--- src/js/userMoments.js
+++ src/js/userMoments.js
@@ -18,11 +18,14 @@ import {
   arrayRemove,
   serverTimestamp,
   Timestamp,
-  deleteDoc
+  deleteDoc,
 } from "firebase/firestore";
 import * as bootstrap from "bootstrap";
 import "../scss/styles.scss";
 import { main, start } from "@popperjs/core";
+
+const axios = require("axios");
+const FormData = require("form-data");
 
 const firebaseConfig = {
   apiKey: "AIzaSyAH168KKUYGhSGV_GVX5SqDGfxm4vtYR7w",
@@ -59,14 +62,16 @@ document.getElementById("logout_btn").addEventListener("click", function () {
     });
 });
 
-
 var editPostModal = new bootstrap.Modal("#editPostModal");
 var closeEditPostModal = document.getElementById("editPostModalBtn");
+var saveCaption = document.getElementById("saveCaption")
 
 closeEditPostModal.addEventListener("click", function () {
   editPostModal.hide();
+});
+saveCaption.addEventListener("click", function () {
+  editPostSightEngieCheck();
 })
-
 
 // clear content
 let main_content = document.getElementById("main_content");
@@ -94,7 +99,7 @@ window.addEventListener("scroll", async function () {
 });
 
 // get data from firestore
-let userPostRef = doc(db, "users", '0y9Kkgd303QrsKSuXzKvqG2DI4E2')
+let userPostRef = doc(db, "users", "0y9Kkgd303QrsKSuXzKvqG2DI4E2");
 const userRef = await getDoc(userPostRef);
 
 let lastVisible; // last loaded post
@@ -108,7 +113,7 @@ async function getMomentsData(amount) {
 
     // Fetch moments for each momentId
     for (const post of posts) {
-      const momentRef = doc(db, "moments", post['momentID']);
+      const momentRef = doc(db, "moments", post["momentID"]);
       const momentSnap = await getDoc(momentRef);
 
       if (momentSnap.exists()) {
@@ -121,7 +126,7 @@ async function getMomentsData(amount) {
           }
         });
       } else {
-        console.log(`Moment with ID ${post['momentID']} not found`);
+        console.log(`Moment with ID ${post["momentID"]} not found`);
       }
     }
   } else {
@@ -218,14 +223,8 @@ if (usersLiked && usersLiked.includes(uid)) {
         <div class="dropdown-content" id="dropdown-content">
           <ul>
             <div>
-              <p>Report Reason:</p>
-              <hr>
-              <li class="reportBtn" id="`+`"data-doc-id ="`+doc.id+`">Hate Speech or Harassment</li>
-              <li class="reportBtn" id="`+`"data-doc-id ="`+doc.id+`">Graphic or Violent Content</li>
-              <li class="reportBtn" id="`+`"data-doc-id ="`+doc.id+`">Misinformation or Fake News</li>
-              <li class="reportBtn" id="`+`"data-doc-id ="`+doc.id+`">Privacy Violation</li>
-              <li class="reportBtn" id="`+`"data-doc-id ="`+doc.id+`">Bullying or Cyberbullying</li>
-              <li class="reportBtn" id="`+`"data-doc-id ="`+doc.id+`">Others</li>
+              <li class="editBtn" id="`+`"data-doc-id ="`+doc.id+`">Edit Post</li>
+              <li class="deleteBtn" id="`+`"data-doc-id ="`+doc.id+`">Delete Post</li>
             </div>
           </ul>
         </div>
@@ -252,12 +251,13 @@ if (usersLiked && usersLiked.includes(uid)) {
   // Add event listeners
   addLikeButtonEventListeners()
   addOpenReportButtonEventListeners()
-  addReportPostButtonEventListener()
   addDownvoteButtonEventListeners()
+  addEditPostButtonEventListener()
+  addDeletePostButtonEventListener()
 }
 
 function removeAllListenersFromClass(elements) {
-  Array.from(elements).forEach(function(element) {
+  Array.from(elements).forEach(function (element) {
     var clonedElement = element.cloneNode(true);
     element.parentNode.replaceChild(clonedElement, element);
   });
@@ -265,6 +265,7 @@ function removeAllListenersFromClass(elements) {
 
 function addLikeButtonEventListeners() {
   let likeButtons = document.getElementsByClassName("likeButton");
+  let downvoteButtons = document.getElementsByClassName("downvoteButton");
   removeAllListenersFromClass(likeButtons)
 
   for (let index = 0; index < likeButtons.length; index++) {
@@ -275,128 +276,203 @@ function addLikeButtonEventListeners() {
       // Your code here, you can use docID, postSpan, likeCount, and elementId as needed
       let likeCount = postSpan.textContent; // Get the content of the <span>
       console.log("OG likeCount:", likeCount);
-      toggleLike(element, docID, postSpan, likeCount);
+      toggleLike(element, downvoteButtons[index], docID, postSpan, likeCount, 0);
     });
   }
 }
 
+function addDownvoteButtonEventListeners() {
+  let likeButtons = document.getElementsByClassName("likeButton");
+  let downvoteButtons = document.getElementsByClassName("downvoteButton");
+  removeAllListenersFromClass(downvoteButtons)
+
+  for (let index = 0; index < downvoteButtons.length; index++) {
+    const element = downvoteButtons[index];
+    const docID = element.dataset.docId; // Get the data-doc-id attribute
+    const postSpan = element.previousElementSibling; // Get the <span> element
+    element.addEventListener("click", function (e) {
+      // Your code here, you can use docID, postSpan, likeCount, and elementId as needed
+      let likeCount = postSpan.textContent; // Get the content of the <span>
+      console.log("OG likeCount:", likeCount);
+      toggleLike(likeButtons[index], element, docID, postSpan, likeCount, 1);
+    });
+  }
+}
 //NEW DROPDOWN FUNCTION
 function addOpenReportButtonEventListeners() {
-    let reportBtn = document.getElementsByClassName('bx-dots-vertical');
-    removeAllListenersFromClass(reportBtn);
+  let reportBtn = document.getElementsByClassName("bx-dots-vertical");
+  removeAllListenersFromClass(reportBtn);
 
-    // Function to close dropdown content if clicked outside
-    function closeDropdownContent() {
-        let dropdowns = document.querySelectorAll('.dropdown-content');
-        dropdowns.forEach(function(dropdown) {
-            if (dropdown.style.display === 'block') {
-                dropdown.style.display = 'none';
-            }
-        });
-    }
-
-    // Add click event listener to document to close dropdowns
-    document.addEventListener('click', function(event) {
-        let isDropdownClick = false;
-        let target = event.target;
-        while (target) {
-            if (target.classList && target.classList.contains('dropdown-content')) {
-                isDropdownClick = true;
-                break;
-            }
-            target = target.parentNode;
-        }
-        if (!isDropdownClick) {
-            closeDropdownContent();
-        }
+  // Function to close dropdown content if clicked outside
+  function closeDropdownContent() {
+    let dropdowns = document.querySelectorAll(".dropdown-content");
+    dropdowns.forEach(function (dropdown) {
+      if (dropdown.style.display === "block") {
+        dropdown.style.display = "none";
+      }
     });
+  }
 
-    for (let index = 0; index < reportBtn.length; index++) {
-        const element = reportBtn[index];
-        element.addEventListener('click', function (e) {
-            console.log(element);
-            const dropDownContentContainerDiv = element.parentNode.nextElementSibling; // get the dropdown content
-            console.log(dropDownContentContainerDiv);
-            if (dropDownContentContainerDiv.style.display === "block") {
-                console.log("Close");
-                dropDownContentContainerDiv.style.display = "none";   
-            } else {
-                console.log("Open");
-                dropDownContentContainerDiv.style.display = "block";
-            }
-            e.stopPropagation(); // Prevents the click event from propagating to the document click event listener
-        });
+  // Add click event listener to document to close dropdowns
+  document.addEventListener("click", function (event) {
+    let isDropdownClick = false;
+    let target = event.target;
+    while (target) {
+      if (target.classList && target.classList.contains("dropdown-content")) {
+        isDropdownClick = true;
+        break;
+      }
+      target = target.parentNode;
     }
+    if (!isDropdownClick) {
+      closeDropdownContent();
+    }
+  });
+
+  for (let index = 0; index < reportBtn.length; index++) {
+    const element = reportBtn[index];
+    element.addEventListener("click", function (e) {
+      console.log(element);
+      const dropDownContentContainerDiv = element.parentNode.nextElementSibling; // get the dropdown content
+      console.log(dropDownContentContainerDiv);
+      if (dropDownContentContainerDiv.style.display === "block") {
+        console.log("Close");
+        dropDownContentContainerDiv.style.display = "none";
+      } else {
+        console.log("Open");
+        dropDownContentContainerDiv.style.display = "block";
+      }
+      e.stopPropagation(); // Prevents the click event from propagating to the document click event listener
+    });
+  }
 }
 
 function addEditPostButtonEventListener() {
-    let editBtn = document.getElementsByClassName('editBtn')
-    removeAllListenersFromClass(editBtn)
-    for (let index = 0; index < editBtn.length; index++) {
+  let editBtn = document.getElementsByClassName("editBtn");
+  removeAllListenersFromClass(editBtn);
+  for (let index = 0; index < editBtn.length; index++) {
     const element = editBtn[index];
-    element.addEventListener('click', function (e) {
-      loadPostModal(element.dataset.docId,element.parentNode.parentNode.parentNode.parentNode.nextElementSibling);
+    element.addEventListener("click", function (e) {
+      loadPostModal(
+        element.dataset.docId,
+        element.parentNode.parentNode.parentNode.parentNode.nextElementSibling
+      );
     });
- }
-}
-
-function loadPostModal(docID,h6Element) {
-    var h6Text = h6Element.innerText;
-    document.getElementById('postTextInput').value = h6Text;
-    editPostModal.show();
-
-}
-function updateCaption(docId) {
-  
-}
-
-function addDeletePostButtonEventListener(){
-    let deleteBtn = document.getElementsByClassName('deleteBtn')
-    removeAllListenersFromClass(deleteBtn)
-    for (let index = 0; index < deleteBtn.length; index++) {
-    const element = deleteBtn[index];
-    element.addEventListener('click', function (e) {
-      console.log(element.dataset.docId)
-      console.log(element.innerHTML)
-      console.log(element.parentNode.parentNode)
-      deletePost(element.dataset.docId,element.parentNode.parentNode)
-      toastMessage("Post has been reported.")
-    });
- }
-}
-
-function toggleLike(likeBtn,docId,postSpan,likeCount){
- if (likeBtn.classList.contains('liked')) {
-    likeBtn.classList.remove('liked');
-    likeBtn.classList.remove('bxs-heart');
-    likeBtn.classList.add('bx-heart');
-    console.log('removed 1 like')
-    console.log("Toggle likecount decrease:",likeCount)
-    updateLikesCount(docId, -1)
-    console.log(postSpan)
-    let likeCtr = parseInt(likeCount)
-    likeCtr --
-    console.log(likeCtr)
-
-    postSpan.innerText = likeCtr;
-
-  } else {
-    likeBtn.classList.add('liked');
-    likeBtn.classList.remove('bx-heart');
-    likeBtn.classList.add('bxs-heart');
-    console.log('added 1 like')
-    console.log("Toggle likecount increase:",likeCount)
-
-    updateLikesCount(docId, 1)
-    console.log(postSpan)
-    //const spanElement = document.getElementById(postSpan); // why is this null lol
-    let likeCtr = parseInt(likeCount)
-    likeCtr ++
-    console.log(likeCtr)
-
-    //console.log(spanElement)
-    postSpan.innerText = likeCtr;
   }
+}
+
+function loadPostModal(docID, h6Element) {
+  var h6Text = h6Element.innerText;
+  document.getElementById("postTextInput").value = h6Text;
+  editPostModal.show();
+}
+function updateCaptionFirebase(docId) {
+  //Firebase update
+}
+
+function addDeletePostButtonEventListener() {
+  let deleteBtn = document.getElementsByClassName("deleteBtn");
+  removeAllListenersFromClass(deleteBtn);
+  for (let index = 0; index < deleteBtn.length; index++) {
+    const element = deleteBtn[index];
+    element.addEventListener("click", function (e) {
+      console.log(element.dataset.docId);
+      console.log(element.innerHTML);
+      console.log(element.parentNode.parentNode);
+      deletePost(element.dataset.docId, element.parentNode.parentNode);
+      toastMessage("Post has been reported.");
+    });
+  }
+}
+
+function toggleLike(likeBtn, downvoteBtn, docId, postSpan, likeCount, type) {
+  // Remove dislike if present and like is clicked
+  if (type === 0) {
+    if (likeBtn.classList.contains('liked')) { // Like to remove like = 0
+      // If already liked, remove like
+      likeBtn.classList.remove('liked');
+      console.log('removed 1 like');
+      // Decrease like count by 1
+      likeCount--;
+      updateLikesCount(docId, -1);
+    } else {
+      if (!likeBtn.classList.contains('liked')) { // Add Like = 1
+        // If not liked, add like
+        likeBtn.classList.add('liked');
+        console.log('added 1 like');
+        // Increase like count by 1
+        likeCount++;
+        updateLikesCount(docId, 1);
+      }
+
+      // Clear dislike if present
+      if (downvoteBtn.classList.contains('disliked')) { // dislike to like = 1 ..... -1 to 1
+        console.log("disliked to liked");
+        // If downvote was already present, clear it
+        likeBtn.classList.add('liked');
+        downvoteBtn.classList.remove('disliked');
+        console.log('removed 1 dislike');
+        // Decrease like count by 1 (to offset the previous increment)
+        likeCount++;
+        // Decrease dislike count by 1
+        updateLikesCount(docId, 1);
+      }
+    }
+  }
+  // Remove like if present and dislike is clicked
+  else if (type === 1) {
+    if (downvoteBtn.classList.contains('disliked')) { // dislike to reset = 0
+      // If downvote is disliked, remove dislike and increment like count
+      downvoteBtn.classList.remove('disliked');
+      console.log('removed 1 dislike');
+      // Decrease dislike count by 1
+      // Increment like count by 1
+      likeCount++;
+      // Update likes count by 1
+      updateLikesCount(docId, 1);
+    } else {
+      // If downvote is not disliked, add dislike and decrement like count
+      if (!downvoteBtn.classList.contains('disliked') && !likeBtn.classList.contains('liked')) { // dislike = -1
+        downvoteBtn.classList.add('disliked');
+        console.log('added 1 dislike');
+        likeCount-= 1;
+        // Update likes count by -1
+        updateLikesCount(docId, -1);
+      }
+      if (likeBtn.classList.contains('liked')) { // Liked to dislike = -1
+        downvoteBtn.classList.add('disliked');
+        likeBtn.classList.remove('liked');
+        console.log("liked and downvoted", 'added 1 dislike');
+        likeCount-= 2;
+        // Update likes count by -1
+        updateLikesCount(docId, -2);
+      }
+    }
+  }
+
+  // Toggle like/dislike icon class
+  if (likeBtn.classList.contains('liked')) {
+    // If liked, set like icon
+    likeBtn.classList.remove('bxs-downvote');
+    likeBtn.classList.add('bxs-upvote');
+    likeBtn.classList.remove('bx-upvote'); // Remove 'bx-upvote' class
+  } else {
+    // If not liked, set default like icon
+    likeBtn.classList.remove('bxs-upvote');
+    likeBtn.classList.add('bx-upvote'); // Add 'bx-upvote' class when neither liked nor disliked
+  }
+
+  if (downvoteBtn.classList.contains('disliked')) {
+    // If disliked, set dislike icon
+    downvoteBtn.classList.remove('bxs-upvote');
+    downvoteBtn.classList.add('bxs-downvote');
+    downvoteBtn.classList.remove('bx-downvote'); // Remove 'bx-downvote' class
+  } else {
+    // If not disliked, set default dislike icon
+    downvoteBtn.classList.remove('bxs-downvote');
+    downvoteBtn.classList.add('bx-downvote'); // Add 'bx-downvote' class when neither liked nor disliked
+  }
+  postSpan.textContent = likeCount;
 }
 
 async function updateLikesCount(docId, num) {
@@ -462,7 +538,6 @@ async function updateLikesCount(docId, num) {
     });
 }
 
-
 async function deletePost(docId, dropDownContentContainerDiv, reason) {
   onAuthStateChanged(auth, async (user) => {
     if (user) {
@@ -475,7 +550,9 @@ async function deletePost(docId, dropDownContentContainerDiv, reason) {
         const userSnap = await getDoc(userRef);
         if (userSnap.exists()) {
           const userData = userSnap.data();
-          const updatedPosts = userData.posts.filter(post => post.momentId !== docId);
+          const updatedPosts = userData.posts.filter(
+            (post) => post.momentId !== docId
+          );
           await setDoc(userRef, { posts: updatedPosts }, { merge: true });
         }
 
@@ -498,3 +575,31 @@ async function deletePost(docId, dropDownContentContainerDiv, reason) {
   });
 }
 
+function editPostSightEngieCheck() {
+  var postTextInput = document.getElementById("postTextInput").value;
+  var caption = postTextInput;
+  var data = new FormData();
+  data.append("text", caption);
+  data.append("lang", "en");
+  data.append("mode", "ml");
+  data.append("api_user", "1227574749");
+  data.append("api_secret", "NLvWeA9iUfYBqg6rMyx6VsaJXy");
+
+  axios({
+    url: "https://api.sightengine.com/1.0/text/check.json",
+    method: "post",
+    data: data,
+    headers: {
+      "Content-Type": "multipart/form-data", 
+    },
+  })
+    .then(function (response) {
+      // on success: handle response
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      if (error.response) console.log(error.response.data);
+      else console.log(error.message);
+    });
+}
